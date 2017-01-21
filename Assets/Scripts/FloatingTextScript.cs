349bc195df72b555d532cd1017ee7310653b9405
Hallgeir Løkken <hallgeir@loekken.org>
2017-01-22 00:31:38 +0100

    Made the floaty-pity bettur

--- Assets/Scripts/FloatingTextScript.cs
+++ Assets/Scripts/FloatingTextScript.cs
@@ -7,10 +7,14 @@ public class FloatingTextScript : MonoBehaviour
 {
     private TextMesh textMesh;
 
-    public Color color;
+    private Color color;
     public int number;
 
-    private float timeToLive = 0.75f;
+    public float timeToLive = 0.3f;
+    public float speed = 5;
+
+    public Color positiveColor = Color.black;
+    public Color negativeColor = Color.black;
 
     void Awake()
     {
@@ -19,14 +23,22 @@ public class FloatingTextScript : MonoBehaviour
 
     void Start()
     {
+        if (number < 0)
+        {
+            color = negativeColor;
+        }
+        else
+        {
+            color = positiveColor;
+        }
         textMesh.color = color;
-        textMesh.text = number.ToString("+#;-#:0");
+        textMesh.text = number.ToString("+#;-#0");
     }
 
     void Update()
     {
         var pos = transform.position;
-        pos.y += Time.deltaTime * 10;
+        pos.y += Time.deltaTime * speed;
 
         transform.position = pos;
 
