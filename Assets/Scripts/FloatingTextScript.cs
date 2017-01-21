395113a5a96e71f980b917ab4f66a44eaa7a241b
Hallgeir Løkken <hallgeir@loekken.org>
2017-01-21 21:48:56 +0100

    Did more

---
+++ Assets/Scripts/FloatingTextScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMesh))]
+public class FloatingTextScript : MonoBehaviour
+{
+    private TextMesh textMesh;
+
+    public Color color;
+    public int number;
+
+    private float timeToLive = 1.5f;
+
+    void Awake()
+    {
+        textMesh = GetComponent<TextMesh>();
+    }
+
+    void Start()
+    {
+        textMesh.color = color;
+        textMesh.text = string.Format("{+0;-#}", number);
+    }
+
+    void Update()
+    {
+        var pos = transform.position;
+        pos.y += Time.deltaTime * 10;
+
+        transform.position = pos;
+
+        timeToLive -= Time.deltaTime;
+        if (timeToLive <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
