6aa5afa8c52be6dbc70c4dc791ffcabc8c1852ce
oskogstad <jorgen.skogstad@gmail.com>
2017-01-21 15:00:53 +0100

    made sandbox, also audiomanager started

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private bool splatSwitch;
+    private List<AudioClip> activeHitScale; // one array per instrument
+    private List<AudioClip> activeLevelCompleteChords; // one per instr
+    private List<AudioClip> backgroundMusic;
+    // lists
+    void Awake()
+    {
+        backgroundMusic = new List<AudioClip>();
+        // load all sfx
+    }
+    
+
+    void LoadSounds(string path, List<AudioClip> sounds)
+    {
+        // loads of load
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void PlaySplat()
+    {
+        if(splatSwitch)
+        {
+            // play 0
+        }
+        else
+        {
+            // play 1
+        }
+
+        splatSwitch = !splatSwitch;
+    }
+}
