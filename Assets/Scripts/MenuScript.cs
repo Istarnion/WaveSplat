ff1608f317ea719ba796846e84f02e1f1ac7196e
Hallgeir Løkken <hallgeir@loekken.org>
2017-01-21 20:52:46 +0100

    Did stuff

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	void Start ()
+    {
+
+        Button btn = GameObject.Find("StartButton").GetComponent<Button>();
+        btn.Select();
+	}
+
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
